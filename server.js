1b79067cf72b5f3b54a921e3100760456527b95a
cunha-rafael <rcunhabh.23@gmail.com>
2026-06-03 16:39:52 -0300

    Adiciona rotas GET POST DELETE na students-api

--- server.js
+++ server.js
@@ -2,8 +2,56 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
+let students = [
+    {
+        id: 1,
+        nome: "Rafael",
+        curso: "ADS"
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        curso: "Engenharia"
+    }
+];
+
+app.get("/students", function (req, res) {
+
+    res.json(students);
+
+});
+
+app.post("/students", function (req, res) {
+
+    const newStudent = {
+        id: students.length + 1,
+        nome: req.body.nome,
+        curso: req.body.curso
+    };
+
+    students.push(newStudent);
+
+    res.status(201).json(newStudent);
+
+});
+
+app.delete("/students/:id", function (req, res) {
+
+    const studentId = Number(req.params.id);
+
+    students = students.filter(function (student) {
+
+    return student.id !== studentId;
+
+});
+
+res.json(students);
+
+}); 
 app.get("/", function (req, res) {
 
     res.send("API de alunos funcionando!");
